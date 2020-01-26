0ad975e4ed1095e76210fae8ff0618e5d341ed54
dfspray <daniel_spray@comcast.net>
2020-01-26 10:39:36 -0800

    Updated the education fields

--- src/main/java/site/portfolio/controllers/MainController.java
+++ src/main/java/site/portfolio/controllers/MainController.java
@@ -5,18 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	  @RequestMapping("/")
+	  public String redirect() {
+	    return "redirect:home";
+	  }
+
 	  @RequestMapping("/home")
 	  public String home() {
 	    return "html/home.html";
 	  }
 	  
-	  @RequestMapping("/about")
-	  public String about() {
-	    return "html/resume.html";
-	  }
-	  
 	  @RequestMapping("/resume")
 	  public String resume() {
 	    return "html/resume.html";
+	  }
+	  
+	  @RequestMapping("/about")
+	  public String about() {
+		  return "html/about.html";
 	  }
 }
